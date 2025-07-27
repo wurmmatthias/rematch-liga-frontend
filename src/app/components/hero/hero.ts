60eb7887dfc9fe4af249d83cc5877d214d81473e
Matthias Wurm <wurm.matthias@gmx.net>
2025-07-27 22:19:26 +0200

    added stream to hero

--- src/app/components/hero/hero.ts
+++ src/app/components/hero/hero.ts
@@ -2,10 +2,11 @@ import { Component, Input } from '@angular/core';
 import { NgIf } from '@angular/common';
 import { NgStyle } from '@angular/common';
 import { NgClass } from '@angular/common';
+import { TwitchPlayer } from '../twitch-player/twitch-player';
 
 @Component({
   selector: 'app-hero',
-  imports: [NgIf, NgStyle, NgClass],
+  imports: [NgIf, NgStyle, NgClass, TwitchPlayer],
   templateUrl: './hero.html',
   styleUrl: './hero.css'
 })
@@ -15,4 +16,11 @@ export class Hero {
   @Input() buttonText?: string;
   @Input() buttonLink?: string;
   @Input() backgroundImage?: string;
+  @Input() showTwitchToggleButton: boolean = false;
+
+  twitchVisible: boolean = false;
+
+  toggleTwitchPlayer(): void {
+    this.twitchVisible = !this.twitchVisible;
+  }
 }
